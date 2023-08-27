1ef55a192c383ad4744af7f237ec9c739fab5d11
Tassio Samuel <tassio.samuel72@gmail.com>
2023-08-27 18:09:01 -0300

    üpdate implamentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@mail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");	
+		seller = sellerDao.findById(1);
+		seller.setName("Maria Whaine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
+		
 }
